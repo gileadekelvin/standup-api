0e75d27b40de9c0cf300f710fa696e265520ffd7
Gileade Kelvin <gileadekelvin@gmail.com>
2022-02-13 15:19:00 -0300

    feat: add graphql linter

--- src/modules/book/Book.loader.ts
+++ src/modules/book/Book.loader.ts
@@ -1,10 +1,12 @@
 export const loadBooks = () => {
   const books = [
     {
+      id: '1',
       title: 'The Awakenings',
       author: 'Kate Chopins',
     },
     {
+      id: '2',
       title: 'City of Glass',
       author: 'Paul John',
     },
