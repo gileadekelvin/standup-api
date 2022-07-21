96c2a8314ab8f733c8c9e59f225c59522991ee68
Gileade Kelvin <gileadekelvin@gmail.com>
2022-07-20 21:15:33 -0300

    refactor: remove template type book

--- packages/api/src/modules/query/Query.resolvers.ts
+++ packages/api/src/modules/query/Query.resolvers.ts
@@ -1,11 +1,7 @@
 import { QueryResolvers } from '../schema';
-import { loadBooks } from '../book/Book.loader';
 import { loadNode } from '../node/Node.loader';
 
 const Query: QueryResolvers = {
-  books: (_, args) => {
-    return loadBooks(args);
-  },
   me: (_, __, ctx) => ctx.dataloaders.User.load(ctx.user.id),
   node: (_, args, ctx) => loadNode(args.id, ctx),
 };
